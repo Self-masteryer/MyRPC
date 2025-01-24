ccbe7bdc3e1b61909fe4097f9703df9a8faccd73
刘成兴 <3338918781@qq.com>
2025-01-25 00:14:08 +0800

    客户端动态代理

---
+++ extend/src/main/java/com/lcx/extend/proxy/ServiceProxy.java
@@ -0,0 +1,46 @@
+package com.lcx.extend.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import com.lcx.extend.model.RpcRequest;
+import com.lcx.extend.model.RpcResponse;
+import com.lcx.extend.serializer.JdkSerializer;
+
+import java.io.IOException;
+
+/**
+ * jdk动态代理
+ */
+public class ServiceProxy implements InvocationHandler {
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        // 指定序列化器
+        JdkSerializer serializer = new JdkSerializer();
+        // 构造请求
+        RpcRequest rpcRequest = RpcRequest.builder()
+                .serviceName(method.getDeclaringClass().getName())
+                .methodName(method.getName())
+                .parameterTypes(method.getParameterTypes())
+                .args(args)
+                .build();
+        try {
+            // 序列化
+            byte[] bodyBytes = serializer.serialize(rpcRequest);
+            // 发送请求
+            // ，这里地址被硬编码了（需要使用注册中心和服务发现机制解决）
+            try (HttpResponse httpResponse = HttpRequest.post("http://localhost:8080")
+                    .body(bodyBytes)
+                    .execute()) {
+                byte[] result = httpResponse.bodyBytes();
+                // 反序列化
+                RpcResponse rpcResponse = serializer.deserialize(result, RpcResponse.class);
+                return rpcResponse.getData();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
