4bccfbfcfe27c6d1b30a5b4302024fd7da244218
刘成兴 <3338918781@qq.com>
2025-01-25 16:52:03 +0800

    序列化器

---
+++ rpc-core/src/main/java/com/lcx/rpc_core/serializer/HessianSerializer.java
@@ -0,0 +1,27 @@
+package com.lcx.rpc_core.serializer;
+
+import com.caucho.hessian.io.HessianInput;
+import com.caucho.hessian.io.HessianOutput;
+
+import java.io.ByteArrayInputStream;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class HessianSerializer implements Serializer {
+
+    @Override
+    public <T> byte[] serialize(T object) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        HessianOutput ho = new HessianOutput(baos);
+        ho.writeObject(object);
+        return baos.toByteArray();
+    }
+
+    @Override
+    public <T> T deserialize(byte[] data, Class<T> clazz) throws IOException {
+        ByteArrayInputStream bais = new ByteArrayInputStream(data);
+        HessianInput hi = new HessianInput(bais);
+        return clazz.cast(hi.readObject());
+    }
+}
