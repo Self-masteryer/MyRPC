9e337689eea2f357283f84103adadd40eed9c9ed
刘成兴 <3338918781@qq.com>
2025-01-25 17:10:38 +0800

    实现动态配置文件

---
+++ rpc-core/src/main/java/com/lcx/rpc_core/config/RpcApplication.java
@@ -0,0 +1,40 @@
+package com.lcx.rpc_core.config;
+
+import com.lcx.rpc_core.utils.ConfigUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RpcApplication {
+
+    private static volatile RpcConfig rpcConfig;
+    private static final Logger log = LoggerFactory.getLogger(RpcApplication.class);
+
+    public static RpcConfig getRpcConfig() {
+        if(rpcConfig == null) {
+            synchronized (RpcApplication.class) {
+                if(rpcConfig == null) {
+                    init();
+                }
+            }
+        }
+        return rpcConfig;
+    }
+
+    private static void init() {
+        RpcConfig newRpcConfig = null;
+        try {
+            // 加载配置文件
+            newRpcConfig = ConfigUtils.loadConfig(RpcConfig.class, "");
+        } catch (Exception e) {
+            // 加载失败，采用默认值
+            rpcConfig = new RpcConfig();
+        }
+        init(newRpcConfig);
+    }
+
+    private static void init(RpcConfig rpcConfig) {
+        RpcApplication.rpcConfig = rpcConfig;
+        log.info("rpc config:{}", rpcConfig);
+    }
+
+}
